6e314e57a69db7a0a0120d8c60999553aad78430
James7911 <smiledog781@gmail.com>
2026-01-20 22:28:13 -0800

    Update MyApp.jsx

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,21 +1,65 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updatedCharacters = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updatedCharacters);
+  function removeOneCharacter(id) {
+    fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.status === 204 || res.status === 200) {
+          setCharacters(characters.filter((character) => character.id !== id));
+        } else if (res.status === 404) {
+          setCharacters(characters.filter((character) => character.id !== id));
+        } else {
+          console.log("Delete failed:", res.status);
+        }
+      })
+      .catch((error) => console.log(error));
   }
 
   function updateList(person) {
-    setCharacters([...characters, person]);
+    postUser(person)
+      .then((res) => {
+        if (res.status === 201) {
+          return res.json();
+        }
+        throw new Error(`Unexpected status: ${res.status}`);
+      })
+      .then((newUser) => {
+        setCharacters([...characters, newUser]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, []);
 
   return (
     <div className="container">
